81cc5f39a48c6451af0b30eeeff84459041f2099
iyrdchtb <sundeepkh@gmail.com>
2017-02-01 18:04:21 -0600

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -5,3 +5,24 @@
 # commit, move on to the rest of the exam.  The object of this problem is for
 # you to understand what the code does without actually running it (ie. do not
 # actually run this).
+
+The code prints value of 10%2 = 0 based on values of i and j. It initializes i and j = 0 and increments then such that j is less than i and i less than 6 based on the two while loops. It prints out values for 10%2 and value of j before incrementing it. (there's print 10%2, j=j+1 in the loop.)
+The outer while increments i; the inner while increments j.
+
+Output
+Nothing printed for i,j=0
+01
+00
+01
+00
+01
+02
+00
+01
+02
+03
+00
+01
+02
+03
+04
