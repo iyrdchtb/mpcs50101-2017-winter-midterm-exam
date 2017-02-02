cf76559ed0e4cfcfc80d9a514f87df559f7ba0a8
iyrdchtb <sundeepkh@gmail.com>
2017-02-01 19:26:11 -0600

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -2,3 +2,26 @@
 # You will have 10 minutes to complete this problem.  You will need to commit
 # this to your repository within the given time.  After you have done your
 # commit, move on to the rest of the exam.
+# You will have 10 minutes to complete this problem.  You will need to commit
+# this to your repository within the given time.  After you have done your
+# commit, move on to the rest of the exam.
+a True
+b False
+c False
+d True
+e True
+f True
+g False
+h True
+i False
+j False
+k True
+l False
+m True
+n False
+o False
+p False
+q True
+r True
+s False
+t False
